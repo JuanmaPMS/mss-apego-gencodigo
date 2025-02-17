7c0458f2f878f74253d847b7a888fc6b167e9a7b
Oscar <ohernandez@grupopm.mx>
2025-02-17 17:48:27 -0600

    Correccion en servicio
    Correccion en servicio

--- GeneraObjetos.py
+++ GeneraObjetos.py
@@ -44,7 +44,7 @@ def RealizaPregunta():
     datos = request.get_json()
     Tablas = datos.get('tablas')
     Pregunta = datos.get('Pregunta')
-    Response = Exec.enviarOPENAI(Tablas, Pregunta)
+    Response = Exec().enviarOPENAI(Tablas, Pregunta)
     return jsonify({"script": Response.replace('```sql','').replace('```','')}) 
 
 @generaObjetos_bp.route('/ER', methods=['GET'])
